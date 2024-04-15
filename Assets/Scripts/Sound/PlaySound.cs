66eb119e38cb8ae80d815dd7f3411769f9672bf7
TYParrot <73172379+TYParrot@users.noreply.github.com>
2024-04-15 18:20:30 +0900

    오디오 소스 추가 및 코드 변경

--- Assets/Scripts/Sound/PlaySound.cs
+++ Assets/Scripts/Sound/PlaySound.cs
@@ -7,16 +7,16 @@ public class PlaySound : MonoBehaviour
     private AudioSource audioSource;
     private bool isPlaying = false;
     private float clipLength = 0;
-    private float minPitch = 0.5f;
-    private float maxPitch = 2.0f;
 
-
-    private Vector3 currentMouse;
-    private Vector3 lastMouse;
+    private Transform objTransform;
+    private Vector3 currentPos;
+    private Vector3 lastPos;
     private float lastCheck;
-    private float mouseDistance;
-    private float mouseSpeed;
+    private float objDistance;
+    private float objSpeed;
     private float checkInterval = 0.1f;
+    
+    private bool pitchOver = false;
     
     
     void Start()
@@ -25,32 +25,60 @@ public class PlaySound : MonoBehaviour
         clipLength = audioSource.clip.length;
         Debug.Log("오디오 클립 길이는 " + clipLength + "초");
 
-        lastMouse = Input.mousePosition;
+        objTransform = GetComponent<Transform>();
+        lastPos = objTransform.position;
         lastCheck = Time.time;
     }
 
     void Update()
     {
-        if(!isPlaying && Input.GetMouseButton(0)){
-            StartCoroutine(PlayAudioWithSpeed());
-        }
-
+        //오디오 pitch 업데이트. 0이면 나오지 않음. 
         if(Time.time - lastCheck >= checkInterval){
-            mouseDistance = Vector3.Distance(lastMouse, currentMouse);
-            mouseSpeed = mouseDistance/checkInterval;
-            mouseSpeed = Mathf.InverseLerp(minPitch, maxPitch, mouseSpeed);
 
-            audioSource.pitch = mouseSpeed;
+            currentPos = objTransform.position;
+            objDistance = Vector3.Distance(lastPos, currentPos);
+            
+            objSpeed = Mathf.Clamp(objDistance, 0.1f, 6);
 
-            lastMouse = currentMouse;
+            if(objSpeed >= 3)   pitchOver = true;
+            else    pitchOver = false;
+
+            //최소, 최대 이동 거리 임의 지정
+            objSpeed = Mathf.InverseLerp(0.1f, 3, objSpeed);
+
+            //0.1~3은 pitch 1로, 3~6은 pitch 2까지 도달 가능
+            if(pitchOver) objSpeed += 1;
+
+            audioSource.pitch = objSpeed;
+
+            lastPos = currentPos;
             lastCheck = Time.time;
         }
 
     }
 
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("DrawableCanvas")){
+            if(!isPlaying){
+                StartCoroutine(PlayAudioWithSpeed());
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        if(other.CompareTag("DrawableCanvas")){
+            if(isPlaying){
+                StopCoroutine(PlayAudioWithSpeed());
+                isPlaying = false;
+            }
+        }
+    }
+
     IEnumerator PlayAudioWithSpeed(){
         isPlaying = true;
-
+        if(audioSource.pitch < 0.5f){
+            audioSource.pitch = 0.5f;
+        }
         audioSource.time = clipLength / 5 * 2; // 재생 위치 설정
         audioSource.Play();
 
